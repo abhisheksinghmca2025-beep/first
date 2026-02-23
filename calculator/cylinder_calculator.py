94e35396fa3642d92e2e77da468278902059371d
abhisheksinghmca2025-beep <abhishek.singh.mca.2025@miet.ac.in>
2026-02-23 20:25:57 +0530

    adding all files

--- calculator/cylinder_calculator.py
+++ calculator/cylinder_calculator.py
@@ -19,3 +19,10 @@ h = float(input("Enter height of cylinder: "))
 print("Curved Surface Area =", cylinder_csa(r, h))
 print("Total Surface Area =", cylinder_tsa(r, h))
 print("Volume =", cylinder_volume(r, h))
+
+# Example Output:
+# Enter radius of cylinder: 3
+# Enter height of cylinder: 5
+# Curved Surface Area = 94.24777960769379
+# Total Surface Area = 150.79644737231007
+# Volume = 141.3716694115407
