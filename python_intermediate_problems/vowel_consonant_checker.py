4a3a16b702941e5f24bf19930cd6f81d3118defa
abhisheksinghmca2025-beep <abhishek.singh.mca.2025@miet.ac.in>
2026-02-24 14:39:44 +0530

    adding files

---
+++ python_intermediate_problems/vowel_consonant_checker.py
@@ -0,0 +1,12 @@
+# Program to check vowel or consonant
+
+ch = input("Enter a character: ")
+
+if ch.lower() in "aeiou":
+    print("Vowel")
+else:
+    print("Consonant")
+
+# Example Output:
+# Enter a character: a
+# Vowel
